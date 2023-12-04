2086220405eb68b331550f6283138f7792ace957
Sinan Aydın <en.snnaydin@gmail.com>
2023-12-04 20:16:46 +0300

    fixed shopping service errors

--- shopping/shopping/src/api/shopping.js
+++ shopping/shopping/src/api/shopping.js
@@ -1,13 +1,12 @@
 const ShoppingService = require("../services/shopping-service");
-const UserService = require('../services/customer-service');
+const { PublishCustomerEvent } = require("../utils");
 const UserAuth = require('./middlewares/auth');
 
 module.exports = (app) => {
     
     const service = new ShoppingService();
-    const userService = new UserService();
 
-    app.post('/shopping/order',UserAuth, async (req,res,next) => {
+    app.post('/order',UserAuth, async (req,res,next) => {
 
         const { _id } = req.user;
         const { txnNumber } = req.body;
@@ -15,6 +14,13 @@ module.exports = (app) => {
 
         try {
             const { data } = await service.PlaceOrder({_id, txnNumber});
+
+            const payload = await service.getOrderPayload(_id, data, 'CREATE_ORDER'); // CREATE_ORDER is in customer service
+
+            PublishCustomerEvent(payload);
+
+
+
             return res.status(200).json(data);
             
         } catch (err) {
@@ -28,8 +34,8 @@ module.exports = (app) => {
         const { _id } = req.user;
 
         try {
-            const { data } = await userService.GetShopingDetails(_id);
-            return res.status(200).json(data.orders);
+            const { data } = await service.GetOrders(_id);
+            return res.status(200).json(data);
         } catch (err) {
             next(err);
         }
@@ -41,8 +47,8 @@ module.exports = (app) => {
 
         const { _id } = req.user;
         try {
-            const { data } = await userService.GetShopingDetails(_id);
-            return res.status(200).json(data.cart);
+            const { data } = await service.GetOrders(_id);
+            return res.status(200).json(data);
         } catch (err) {
             next(err);
         }
